e0733e3dafe498bd9d1fe93f5947430bbbac8e2f
sahibzadafahad-whizpool <sahibzada.fahad@whizpool.com>
2022-01-30 13:09:44 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-UILD_ID=dontKillMe
-nohup java -jar target/myproject-0.0.1-SNAPSHOT.jar &
+set +e #so "at now" will run even if java -jar fails
+#Run java app in background
+echo "java -jar $(ls | grep myproject-0.0.1-SNAPSHOT.jar | head -n 1)" | at now + 1 min
