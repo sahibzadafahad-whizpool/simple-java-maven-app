3478e9e8a34bd6127890fec54c634fa88b49087d
sahibzadafahad-whizpool <sahibzada.fahad@whizpool.com>
2022-01-30 12:28:15 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export BUILD_ID=dontKillMe
-nohup java -jar target/myproject-0.0.1-SNAPSHOT.jar --server.port=8080 &
+nohup java -jar target/myproject-0.0.1-SNAPSHOT.jar  > /var/lib/jenkins/data.txt 2>&1 &
 sleep 5
