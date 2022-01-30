497329e82d9de3cb4c5e7526abc274d7532bc22f
sahibzadafahad-whizpool <sahibzada.fahad@whizpool.com>
2022-01-30 12:58:49 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup java -jar target/myproject-0.0.1-SNAPSHOT.jar  > /var/lib/jenkins/workspace/simple-java-maven-app@2/simple-java-maven-app/nohup.out 2>&1 &
+java -jar target/myproject-0.0.1-SNAPSHOT.jar &
 sleep 5
