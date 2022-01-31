6e6097917173d700a9d19e879cb40ccc863a5843
sahibzadafahad-whizpool <sahibzada.fahad@whizpool.com>
2022-01-31 15:43:28 +0500

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World";
+        return "Hello World1";
     }
 
     public static void main(String[] args) throws Exception {
